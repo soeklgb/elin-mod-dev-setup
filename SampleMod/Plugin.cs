c56d19b53f043642a6f5d3ca833a37db69ec5385
enzaki <96245674+soeklgb@users.noreply.github.com>
2024-12-07 13:55:27 +0900

    wip

--- SampleMod/Plugin.cs
+++ SampleMod/Plugin.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 [BepInPlugin("author.sample_mod", "Sample Mod", "1.0.0")]
 public class Plugin : BaseUnityPlugin
 {
-    public void OnStartCore()
+    public void Awake()
     {
         var harmony = new Harmony("author.sample_mod");
         harmony.PatchAll();
